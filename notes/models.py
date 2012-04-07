741b9355e2c90cbbea91e4cd9d633d06295f11c2
David Brodsky <davidpbrodsky@gmail.com>
2012-04-07 10:50:11 -0700

    move models and views to notes app folder

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+class School(models.Model):
+    name = models.CharField()
+    location = models.CharField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+class Course(models.Model):
+    school = models.ForeignKey(School, blank=True, null=True)
+    title = models.CharField()
+    field = models.CharField(blank=True)
+    academic_year = models.IntegerField(blank=True, null=True)
+    #professor = models.ForeignKey()
+    def __unicode__(self):
+        return self.title, self.school
+
+class Notes(models.Model):
+    title = models.CharField()
+    course = models.ForeignKey(Course, blank=True, null=True)
+    school = models.ForeignKey(School, blank=True, null=True)
+    html = models.TextField(blank=True, null=True)
+
+class UserProfile(models.Model):
+    """ User objects have the following fields:
+
+        username
+        first_name
+        last_name
+        email
+        password
+        is_staff
+        is_active
+        is_superuser
+        last_login
+        date_joined
+
+        user_profile extends the user to add our extra fields
+    """
+    ## 1-to-1 relation to user model
+    # This field is required
+    user = models.ForeignKey(User, unique=True)
+    school = models.ForeignKey(School, blank=True, null=True)
+    karma = models.IntegerField(default=0)
+
+    # Optional fields:
+    gravatar = models.URLField(blank=True) # Profile glitter
+    grad_year = models.CharField(blank=True, null=True)
+    fb_id = models.CharField(blank=True, null=True)
+    can_upload = models.BooleanField()
+    can_read = models.BooleanField()
+    can_vote = models.BooleanField()
+    can_comment = models.BooleanField()
+    can_moderate = models.BooleanField()
+
+def ensure_profile_exists(sender, **kwargs):
+    if kwargs.get('created', False):
+        UserProfile.objects.create(user=kwargs.get('instance'))
+
+post_save.connect(ensure_profile_exists, sender=User)
