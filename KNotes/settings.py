b1a46f08dd11f440d5118be29b25360f300e85ea
Seth Woodworth <seth@sethish.com>
2012-04-15 19:48:46 -0400

    removing django.contrib.sites since we aren't using it and it breaks on a new deploy with south

--- KNotes/settings.py
+++ KNotes/settings.py
@@ -39,8 +39,6 @@ TIME_ZONE = 'America/Chicago'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
-SITE_ID = 1
-
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
@@ -175,7 +173,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
