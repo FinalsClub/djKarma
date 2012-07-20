a6576ad6e9ff7f46553fbe51ecd9facc9c2836fe
David Brodsky <davidpbrodsky@gmail.com>
2012-07-19 17:15:34 -0700

    check_file_ownership bugfix and search result tweaks

--- notes/templatetags/check_file_ownership.py
+++ notes/templatetags/check_file_ownership.py
@@ -9,6 +9,8 @@ def ownedBy(file, user_pk):
     """ Returns True if the given file is owned by the user
         with pk=user_pk. False otherwise
     """
+    if user_pk == -1:
+        return False
     if file.owner == User.objects.get(pk=user_pk) or User.objects.get(pk=user_pk).get_profile().files.filter(pk=file.pk).exists():
         return True
     return False
