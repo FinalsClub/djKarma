d3bddae2b936710bd6aad85926ca6b82bdc91076
Seth Woodworth <seth@sethish.com>
2012-11-12 18:35:38 -0500

    lightbox behavior and fix positioning

--- notes/static/js/app.js
+++ notes/static/js/app.js
@@ -1,8 +1,30 @@
 $(document).ready(function(){
 
 // Click Handlers
+  // Toggle: Login box dropdown
   $("span#login_toggle").click(function(){
     $("#login_box").toggle();
+  });
+
+  // Toggle: my courses dropdown
+  $("#global_header_mycourses_copy").click(function(){
+    $("#global_header_mycourses_list").toggle();
+    $("#global_header_add_course").toggle();
+  });
+
+  // Toggle: avatar menu dropdown
+  $("#global_header_avatar_menu").click(function(){
+    $("#global_header_avatar_moreinfo").toggle();
+  });
+
+  // Show Signup lightbox
+  $("#login_signup").click(function() {
+    $("#lightbox_signup").toggle();
+  });
+
+  // Dismiss all lightboxes
+  $(".lightbox_close").click(function() {
+    $(".modal_content").hide();
   });
 
 
