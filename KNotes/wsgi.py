8bcdd112a5c37a16e061190a04f717c6f4149644
Seth Woodworth <seth@sethish.com>
2012-12-19 16:37:47 -0500

    wsgi.py is not copyrightable, remove declaration

--- KNotes/wsgi.py
+++ KNotes/wsgi.py
@@ -1,21 +1,5 @@
-# wsgi.py is part of Karma Notes
-# Copyright (C) 2012  FinalsClub Foundation
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 """
-WSGI config for KNotes project.
+WSGI config for djKarma project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
